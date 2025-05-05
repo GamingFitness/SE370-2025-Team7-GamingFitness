c754d72c3cd8119d82cda797423c8a853c4a0d54
Judah-F <Judahfisher21@gmail.com>
2025-05-04 18:15:45 -0700

    Update signup.js
    changing it so that the newly created use is POSTed to the database

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -40,5 +40,19 @@ function signup() {
     confirmPasswordInput.style.display = "none";
     confirmPasswordInput.value = "";
 
+ // Using a POST Request to store this user data in the database
+ fetch('http://localhost:8080/app_users', { //making a fetch request to pull all the users
+    method: 'POST', //we have to declare the POST method to use it. A POST creates a new instance, while a PUT request changes whats there
+    //method: 'PATCH',
+    headers: { 
+    'Content-Type': 'application/json' //declaring content type as json
+  },
+  body: JSON.stringify({ //this is setting up the JSON we want to pass
+    userName: username, //this sets up the user's userName
+    password: password, //setting password, NOT TECHNICALLY SECURE
+    //style: 'BodyBuilding', //setting user style
+    //shots: SHOTS
+  })
+})
     closeLogin();
 }
